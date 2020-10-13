03beee334533637bb424c4fc90db1353743dd60c
veteral <alexzpv@yandex.ru>
2020-10-13 11:36:09 +0300

    add new structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+//const {v4} = require('uuid')
+const app = express()
+
+app.use(express.json())
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+app.listen(5000, () => console.log('Сервер был запущен на порту 5000...'))
